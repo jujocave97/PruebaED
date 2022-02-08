69fd8d43d340189fecb2972439005b23d42556f8
Juanjose <juanjose.cabral.vega.alu@iesfernandoaguilar.es>
2022-02-08 09:16:03 +0100

    3cer comite de bienvenida

--- Entornitos.java
+++ Entornitos.java
@@ -1,10 +1,16 @@
 public class Entornitos{
 	public static void main(String [] args) {
 		System.out.println("Aqui me hayo haciendo esto");
-		int m=20;
-		for (int i=0;i<0 ;i-- ) {
+		int juan=0;
+		for (int i=20;i<0 ;i-- ) {
 			System.out.println(i);
-			System.out.println("Juanjo gay");
+			System.out.println("Juan");
 		}
-	}
+
+		do{
+			System.out.println("paco pepe");
+			juan++;
+		}while (juan==300) ;
+			
+		
 }
