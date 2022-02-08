1eddad74c5c37debe514a38549bf20e20bd0dc1d
Juanjose <juanjose.cabral.vega.alu@iesfernandoaguilar.es>
2022-02-08 09:02:32 +0100

    Segundo commite

--- Entornitos.java
+++ Entornitos.java
@@ -1,5 +1,9 @@
 public class Entornitos{
 	public static void main(String [] args) {
 		System.out.println("Aqui me hayo haciendo esto");
+		int m=0;
+		for (int i=0;i<20 ;i++ ) {
+			System.out.println(i);
+		}
 	}
 }
